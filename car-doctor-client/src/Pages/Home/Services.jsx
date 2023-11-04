61c1e442fb750c1179598a707a38092f8d86ba98
abircu <mdabircse4873@gmail.com>
2023-11-04 14:28:03 +0600

    car-doctor-setup

---
+++ car-doctor-client/src/Pages/Home/Services.jsx
@@ -0,0 +1,20 @@
+const Services = () => {
+  return (
+    <div className="py-4 md:py-6 lg:py-10">
+      <h1 className="text-xl font-semibold text-red-500 text-center mt-10">
+        Services
+      </h1>
+      <h2 className="text-3xl font-bold text-center">Our Service Area</h2>
+      <p className="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Cumque
+        adipisci porro eum? Nostrum tempora reprehenderit dolorum non illo sed
+        eius,
+        <br />
+        obcaecati sit quae earum, quidem exercitationem <br />
+        cumque commodi cum itaque.
+      </p>
+    </div>
+  );
+};
+
+export default Services;
--- car-doctor-client/src/main.jsx
+++ car-doctor-client/src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Router/Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className="max-w-7xl mx-auto ">
+  <div className="max-w-7xl mx-auto  bg-base-200">
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>

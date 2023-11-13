370f25a0e71cd63312d25f9c322399012507468e
abircu <mdabircse4873@gmail.com>
2023-11-14 00:03:56 +0530

    server site data updated

--- car-doctor-server/index.js
+++ car-doctor-server/index.js
@@ -57,7 +57,7 @@ async function run() {
       res.send(result)
     })
     // update data 
-    app.patch('/bookings/:id',async(req,res)=>{
+    app.patch('/bookings/:id',async(req, res)=>{
       const id=req.params.id;
       const filter={_id: new ObjectId(id)}
       const updatedBooking=req.body;
